2ab0fd65239ffb9239eb0bf51417dd7a92250de4
koladeore <koladeore@gmail.com>
2022-06-02 13:46:06 +0100

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './NavBar';
+import Footer from './Footer';
+
+const Layout = ({children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Ore Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
